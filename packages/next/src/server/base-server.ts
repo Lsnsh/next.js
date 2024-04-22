eb88d19dda9d2edb8c149033d37b027bfd1836f8
Janka Uryga <lolzatu2@gmail.com>
2024-04-22 15:09:33 +0200

    make after() work in route handlers

--- packages/next/src/server/base-server.ts
+++ packages/next/src/server/base-server.ts
@@ -2384,6 +2384,8 @@ export default abstract class Server<
               supportsDynamicHTML,
               incrementalCache,
               isRevalidate: isSSG,
+              // @ts-expect-error TODO(after): fix the typing here
+              waitUntil: this.getWaitUntil(res),
             },
           }
 
