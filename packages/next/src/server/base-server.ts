048991da08747341118dfddd8313806f6535419d
Janka Uryga <lolzatu2@gmail.com>
2024-05-08 23:44:46 +0200

    fix: after() in edge functions (middleware and route handlers)

--- packages/next/src/server/base-server.ts
+++ packages/next/src/server/base-server.ts
@@ -2390,7 +2390,6 @@ export default abstract class Server<
               supportsDynamicHTML,
               incrementalCache,
               isRevalidate: isSSG,
-              // @ts-expect-error TODO(after): fix the typing here
               waitUntil: this.getWaitUntil(),
               onClose: res.onClose.bind(res),
             },
